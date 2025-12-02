b69c1ea55a8149cff930ef2d2eea8d185a57f36d
Alex Tamai <tamaia2@mailbox.winthrop.edu>
2025-12-02 08:03:58 -0500

    created new intial py commit

---
+++ image_detection.py
@@ -0,0 +1,11 @@
+######################################################
+#  Student Name: Alex Tamai
+#  Student ID: W30591647
+#  Course Code: CSCI 460 -- Fall 2025
+#  Assignment Due Date: December 1, 2025
+#  GitHub Link: https://github.com/tamaia2/CSCI460F2025_W30591647_Programing_Assignemnt2
+######################################################
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
